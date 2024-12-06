5570ff8104ef581f90cdd3db116ab4f7e6239f19
Henri Francois <henri.francois@protonmail.com>
2024-12-05 19:23:29 -0500

    created posts and labels

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,41 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
-export const LABEL_LIMIT = 1;
+export const DELETE = '3lcluwtqdr224';
+export const LABEL_LIMIT = 3;
 export const LABELS: Label[] = [
   {
-    rkey: '3lcgswmfzv722',
-    identifier: 'tekken8',
+    rkey: '3lcluwt4e4c2c',
+    identifier: 'tekken-8',
     locales: [
       { lang: 'en', name: 'Tekken 8', description: 'Bandai Namco\' Tekken 8'},
     ]
   },
   {
-    rkey: '3lcgswminpg2u',
-    identifier: 'street-fighter6',
+    rkey: '3lcluwt7dss2z',
+    identifier: 'street-fighter-6',
     locales: [
       { lang: 'en', name: 'Street Fighter 6', description: 'Capcom\'s Street Fighter 6'},
+    ]
+  },
+  {
+    rkey: '3lcluwthlic2j',
+    identifier: 'mortal-kombat-1',
+    locales: [
+      { lang: 'en', name: 'Mortal Kombat 1', description: 'Mortal Kombat 1'},
+    ]
+  },
+  {
+    rkey: '3lcluwtkkby25',
+    identifier: 'guilty-gear-strive',
+    locales: [
+      { lang: 'en', name: 'Guilty Gear - Strive', description: 'Guilty Gear - Strive'},
+    ]
+  },
+  {
+    rkey: '3lcluwtnfz224',
+    identifier: 'grandblue-fantasy-versus-rising',
+    locales: [
+      { lang: 'en', name: 'Grandblue Fantasy Versus: Rising', description: 'Grandblue Fantasy Versus: Rising'},
     ]
   }
 ];
