76f505c821802f0c1b963f3d5215e7ba7ce1593a
Henri Francois <henri.francois@protonmail.com>
2025-01-17 22:12:32 -0500

    updated rkeys

--- src/constants.ts
+++ src/constants.ts
@@ -4,35 +4,35 @@ export const DELETE = '3lcluwtqdr224';
 export const LABEL_LIMIT = 3;
 export const LABELS: Label[] = [
   {
-    rkey: '3lcluwt4e4c2c',
+    rkey: '3lfydklrnoc2p',
     identifier: 'tekken-8',
     locales: [
       { lang: 'en', name: 'Tekken 8', description: 'Bandai Namco\' Tekken 8'},
     ]
   },
   {
-    rkey: '3lcluwt7dss2z',
+    rkey: '3lfydklupyg22',
     identifier: 'street-fighter-6',
     locales: [
       { lang: 'en', name: 'Street Fighter 6', description: 'Capcom\'s Street Fighter 6'},
     ]
   },
   {
-    rkey: '3lcluwthlic2j',
+    rkey: '3lfydklxy3p25',
     identifier: 'mortal-kombat-1',
     locales: [
       { lang: 'en', name: 'Mortal Kombat 1', description: 'Mortal Kombat 1'},
     ]
   },
   {
-    rkey: '3lcluwtkkby25',
+    rkey: '3lfydkm343o22',
     identifier: 'guilty-gear-strive',
     locales: [
       { lang: 'en', name: 'Guilty Gear - Strive', description: 'Guilty Gear - Strive'},
     ]
   },
   {
-    rkey: '3lcluwtnfz224',
+    rkey: '3lfydkm653k2p',
     identifier: 'grandblue-fantasy-versus-rising',
     locales: [
       { lang: 'en', name: 'Grandblue Fantasy Versus: Rising', description: 'Grandblue Fantasy Versus: Rising'},
