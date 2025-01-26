d857e03754f60a96d54ba9c397873752e7cbf758
Henri Francois <henri.francois@protonmail.com>
2025-01-25 23:26:48 -0500

    ...

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Bot, Post } from '@skyware/bot';
 import { HOST, PORT, BSKY_IDENTIFIER, BSKY_PASSWORD } from './config.js';
 import { labelerServer } from './labeler.js';
 import logger from './logger.js';
-import { LABELS } from './constants.js';
+import { DELETE, LABELS } from './constants.js';
 
 const bot = new Bot();
 await bot.login({
@@ -21,10 +21,19 @@ labelerServer.start({ port: PORT, host: HOST }, (error, address) => {
 
 bot.on("like", async ({ subject, user }) => {
   if (subject instanceof Post) {
-    const label = LABELS.find((label) => label.rkey === subject.uri.split('/').pop());
-    if (label) {
-      logger.info(`User ${user.did} liked post ${subject.uri}, labeling with ${label.identifier}`);
-      await user.labelProfile([label.identifier])
+    const rkey = subject.uri.split('/').pop();
+    switch (rkey) {
+      case DELETE:
+        logger.info(`User ${user.did} liked post ${subject.uri}, deleting all previous labels`);
+        await user.negateProfileLabels(LABELS.map((label) => label.identifier));
+        break;
+      default:
+        const label = LABELS.find((label) => label.rkey === subject.uri.split('/').pop());
+        if (label) {
+          logger.info(`User ${user.did} liked post ${subject.uri}, labeling with ${label.identifier}`);
+          await user.labelProfile([label.identifier])
+        }
+        break;
     }
   }
 });
