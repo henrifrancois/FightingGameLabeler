945d890d60117839fc7c2f5405e241d5e224c45e
Henri Francois <henri.francois@protonmail.com>
2025-01-25 23:15:11 -0500

    changed label event handling, AGAIN

--- src/labeler.ts
+++ src/labeler.ts
@@ -6,86 +6,9 @@ import logger from './logger.js';
 
 export const labelerServer = new LabelerServer({ did: DID, signingKey: SIGNING_KEY });
 
-export const label = async (did: string, rkey: string) => {
-  logger.info(`Received rkey: ${rkey} for ${did}`);
 
-  if (rkey === 'self') {
-    logger.info(`${did} liked the labeler. Returning.`);
-    return;
-  }
-  try {
-    const labels = await fetchCurrentLabels(did);
 
-    if (rkey.includes(DELETE)) {
-      deleteAllLabels(did, labels);
-    } else {
-      addOrUpdateLabel(did, rkey, labels);
-    }
-  } catch (error) {
-    logger.error(`Error in \`label\` function: ${error}`);
-  }
-};
 
-async function fetchCurrentLabels(did: string) {
-  const query = await labelerServer.db
-    .execute({
-      sql: 'SELECT * FROM labels WHERE uri = ?',
-      args: [did]
-    })
-  const labels = query.rows.reduce((set, row) => {
-    if (row.val) {
-      if (!row.neg) set.add(row.val.toString());
-      else set.delete(row.val?.toString());
-      return set;
-    } else {
-      return new Set<string>()
-    }
-  }, new Set<string>())
 
-  if (labels.size > 0) {
-    logger.info(`Current labels: ${Array.from(labels).join(', ')}`);
-  }
 
-  return labels;
-}
 
-function deleteAllLabels(did: string, labels: Set<string>) {
-  const labelsToDelete: string[] = Array.from(labels);
-
-  if (labelsToDelete.length === 0) {
-    logger.info(`No labels to delete`);
-  } else {
-    logger.info(`Labels to delete: ${labelsToDelete.join(', ')}`);
-    try {
-      labelerServer.createLabels({ uri: did }, { negate: labelsToDelete });
-      logger.info('Successfully deleted all labels');
-    } catch (error) {
-      logger.error(`Error deleting all labels: ${error}`);
-    }
-  }
-}
-
-function addOrUpdateLabel(did: string, rkey: string, labels: Set<string>) {
-  const newLabel = LABELS.find((label) => label.rkey === rkey);
-  if (!newLabel) {
-    logger.warn(`New label not found: ${rkey}. Likely liked a post that's not one for labels.`);
-    return;
-  }
-  logger.info(`New label: ${newLabel.identifier}`);
-
-  if (labels.size >= LABEL_LIMIT) {
-    try {
-      labelerServer.createLabels({ uri: did }, { negate: Array.from(labels) });
-      logger.info(`Successfully negated existing labels: ${Array.from(labels).join(', ')}`);
-    } catch (error) {
-      logger.error(`Error negating existing labels: ${error}`);
-    }
-  }
-
-  try {
-    labelerServer.createLabel({ uri: did, val: newLabel.identifier });
-    logger.info(`Successfully labeled ${did} with ${newLabel.identifier}`);
-  } catch (error) {
-    logger.error(`Error adding new label: ${error}`);
-  }
-}
--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,17 @@
-import { CommitCreateEvent, Jetstream } from '@skyware/jetstream';
 import { Bot, Post } from '@skyware/bot';
-import fs from 'node:fs';
 
-import { CURSOR_UPDATE_INTERVAL, DID, FIREHOSE_URL, HOST, METRICS_PORT, PORT, WANTED_COLLECTION, BSKY_IDENTIFIER, BSKY_PASSWORD } from './config.js';
-import { label, labelerServer } from './labeler.js';
+import { HOST, PORT, BSKY_IDENTIFIER, BSKY_PASSWORD } from './config.js';
+import { labelerServer } from './labeler.js';
 import logger from './logger.js';
-import { startMetricsServer } from './metrics.js';
 import { LABELS } from './constants.js';
 
-let cursor = 0;
-let cursorUpdateInterval: NodeJS.Timeout;
 const bot = new Bot();
 await bot.login({
   identifier: BSKY_IDENTIFIER,
   password: BSKY_PASSWORD,
 });
 
-function epochUsToDateTime(cursor: number): string {
-  return new Date(cursor / 1000).toISOString();
-}
-
-try {
-  logger.info('Trying to read cursor from cursor.txt...');
-  cursor = Number(fs.readFileSync('cursor.txt', 'utf8'));
-  logger.info(`Cursor found: ${cursor} (${epochUsToDateTime(cursor)})`);
-} catch (error) {
-  if (error instanceof Error && 'code' in error && error.code === 'ENOENT') {
-    cursor = Math.floor(Date.now() * 1000);
-    logger.info(`Cursor not found in cursor.txt, setting cursor to: ${cursor} (${epochUsToDateTime(cursor)})`);
-    fs.writeFileSync('cursor.txt', cursor.toString(), 'utf8');
-  } else {
-    logger.error(error);
-    process.exit(1);
-  }
-}
-
-const jetstream = new Jetstream({
-  wantedCollections: [WANTED_COLLECTION],
-  endpoint: FIREHOSE_URL,
-  cursor: cursor,
-});
-
-jetstream.on('open', () => {
-  logger.info(
-    `Connected to Jetstream at ${FIREHOSE_URL} with cursor ${jetstream.cursor} (${epochUsToDateTime(jetstream.cursor!)})`,
-  );
-  cursorUpdateInterval = setInterval(() => {
-    if (jetstream.cursor) {
-      logger.info(`Cursor updated to: ${jetstream.cursor} (${epochUsToDateTime(jetstream.cursor)})`);
-      fs.writeFile('cursor.txt', jetstream.cursor.toString(), (err) => {
-        if (err) logger.error(err);
-      });
-    }
-  }, CURSOR_UPDATE_INTERVAL);
-});
-
-jetstream.on('close', () => {
-  clearInterval(cursorUpdateInterval);
-  logger.info('Jetstream connection closed.');
-});
-
-jetstream.on('error', (error) => {
-  logger.error(`Jetstream error: ${error.message}`);
-});
-
-jetstream.onCreate(WANTED_COLLECTION, (event: CommitCreateEvent<typeof WANTED_COLLECTION>) => {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (event.commit?.record?.subject?.uri?.includes(DID)) {
-    label(event.did, event.commit.record.subject.uri.split('/').pop()!);
-  }
-});
-
-const metricsServer = startMetricsServer(METRICS_PORT);
-
-labelerServer.app.listen({ port: PORT, host: HOST }, (error, address) => {
+labelerServer.start({ port: PORT, host: HOST }, (error, address) => {
   if (error) {
     logger.error('Error starting server: %s', error);
   } else {
@@ -85,20 +23,17 @@ bot.on("like", async ({ subject, user }) => {
   if (subject instanceof Post) {
     const label = LABELS.find((label) => label.rkey === subject.uri.split('/').pop());
     if (label) {
+      logger.info(`User ${user.did} liked post ${subject.uri}, labeling with ${label.identifier}`);
       await user.labelProfile([label.identifier])
     }
   }
 });
 
-jetstream.start();
 
 function shutdown() {
   try {
     logger.info('Shutting down gracefully...');
-    fs.writeFileSync('cursor.txt', jetstream.cursor!.toString(), 'utf8');
-    jetstream.close();
     labelerServer.stop();
-    metricsServer.close();
   } catch (error) {
     logger.error(`Error shutting down gracefully: ${error}`);
     process.exit(1);
